5b3824f6aa6952ea940d092ecba8b4b5bb6362b0
sahuji90 <sahusunil90@gmail.com>
2024-10-07 20:57:58 +0530

    initial commit

---
+++ src/main/java/guru/springframework/spring5webapp2024/Spring5webapp2024Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5webapp2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webapp2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webapp2024Application.class, args);
+	}
+
+}
